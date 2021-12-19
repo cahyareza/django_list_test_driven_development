c2dd1f5e47375a77a3d3278be9e04c4f99cee7a6
cahyafaisalreza <cf.reza07@gmail.com>
2021-12-20 00:17:20 +0700

    Part 2 chapter 16
    add jquery, qunits, lits.js with keypress listener

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,1 +1,8 @@
-$('.has-error').hide();
+var initialize = function () {
+//    console.log('initialize called');
+    $('input[name="text"]').on('keypress', function () {
+//        console.log('in keypress handler');
+        $('.has-error').hide();
+    });
+};
+//console.log('list.js loaded');
