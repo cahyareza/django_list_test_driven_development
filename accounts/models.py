6b87ff0fb41f8ed4d5db78e7d7b70d4767bf03cb
cahyafaisalreza <cf.reza07@gmail.com>
2021-12-23 15:52:45 +0700

    Subclassing abstract user

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser):
     email = models.EmailField(primary_key=True)
+    username = models.CharField(blank=True, null=True, max_length=50)
+    password = models.CharField(blank=True, null=True, max_length=50)
 
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
     USERNAME_FIELD = 'email'
     is_anonymous = False
     is_authenticated = True
+
+    def __str__(self):
+        return self.email
 
 class Token(models.Model):
     email = models.EmailField()
--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -4,6 +4,37 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(email='normal@user.com', password='foo', username='normal')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertEqual(user.username, 'normal')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password="foo", username='')
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='super@user.com', password='foo', username='super')
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertEqual(admin_user.username, 'super')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', password='foo', username='', is_superuser=False)
+
+
 class UserModelTest(TestCase):
 
     def test_user_is_valid_with_email_only(self):
